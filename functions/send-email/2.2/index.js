a886f1a47d5e58bbc15548d96b5aceb21037bb60
Sam <sammiedegroot@gmail.com>
2024-01-10 17:23:36 +0100

    Version 2.2
    This version allows the user to select a collection where the files that need to be sent as attachment are stored. Useful when multiple files need to be added as an attachment dynamically

---
+++ functions/send-email/2.2/index.js
@@ -0,0 +1,97 @@
+import _ from 'lodash';
+import Liquid from './liquid.min';
+
+const mapAttachments = (attachmentsMap, attachmentsCol, attachmentProp) => {
+  const attachments = [];
+  attachmentsMap.map(({ key, value }) => {
+    attachments.push({ filename: key, path: value && value.url ? value.url : value });
+  });
+
+  if (attachmentsCol && attachmentsCol.data) {
+    const prop = attachmentProp[0].name;
+    attachmentsCol.data.map((item) => {
+      const fileName = item[prop].name;
+      const fileUrl = item[prop].url;
+      attachments.push({ filename: fileName, path: fileUrl });
+    });
+  }
+
+  return attachments;
+};
+
+
+const sendEmail = async ({
+  host,
+  port,
+  username,
+  password,
+  secure,
+  senderEmail,
+  senderName,
+  toEmail,
+  toName,
+  cc,
+  bcc,
+  subject,
+  body,
+  variables,
+  attachments,
+  attachmentsCol,
+  attachmentsColProperty,
+  replyTo,
+}) => {
+  const smtpCredentials = {
+    host,
+    port,
+    username,
+    password,
+    secure,
+  };
+
+
+  const engine = new Liquid();
+
+  engine.registerFilter('group', (collection, key) => _.groupBy(collection, key));
+
+  const renderedBody = engine.parseAndRenderSync(
+    body,
+    variables.reduce((ctx, { key, value }) => {
+      ctx[key] = value;
+      return ctx;
+    }, {}),
+  );
+
+  // throw JSON.stringify(variables);
+  // const variableMap = variables.reduce((previousValue, currentValue) => {
+  //   previousValue[currentValue.key] = currentValue.value;
+  //   return previousValue;
+  // }, {});
+
+
+  const mappedAttachments = mapAttachments(attachments, attachmentsCol, attachmentsColProperty);
+
+  const mail = {
+    sender: {
+      from: senderName ? `"${senderName}" ${senderEmail}` : senderEmail,
+      name: senderName,
+      replyTo,
+    },
+    recipient: {
+      to: toName ? `"${toName}" ${toEmail}` : toEmail,
+      cc: cc,
+      bcc: bcc,
+    },
+    subject,
+    body: renderedBody,
+    attachments: mappedAttachments,
+  };
+
+
+
+  const sentMail = await smtp(smtpCredentials, mail);
+
+  return {
+    result: sentMail,
+  };
+};
+export default sendEmail;
