0f60ccd4e8f75033305b7ebc356d1e5da24f3ed7
Rob Dekker <rob-ajax@live.nl>
2024-12-02 16:47:48 +0100

    feat: add advanced boolean option to send email asynchronously

---
+++ functions/send-email/2.4/index.js
@@ -0,0 +1,107 @@
+import _ from 'lodash';
+import Liquid from '../../utils/liquid.min';
+
+const mapAttachments = (attachmentsMap, attachmentsCol, attachmentProp) => {
+  const attachments = [];
+  attachmentsMap.map(({ key, value }) => {
+    if (!value) return;
+    if (!key) return;
+    attachments.push({
+      filename: key,
+      path: value && value.url ? value.url : value,
+    });
+  });
+
+  if (attachmentsCol && attachmentsCol.data) {
+    const prop = attachmentProp[0].name;
+    attachmentsCol.data.map((item) => {
+      const fileName = item[prop].name;
+      const fileUrl =
+        item[prop].url && item[prop].url ? item[prop].url : item[prop];
+      attachments.push({ filename: fileName, path: fileUrl });
+    });
+  }
+
+  return attachments;
+};
+
+const sendEmail = async ({
+  host,
+  port,
+  username,
+  password,
+  secure,
+  senderEmail,
+  senderName,
+  toEmail,
+  toName,
+  cc,
+  bcc,
+  subject,
+  body,
+  variables,
+  attachments,
+  attachmentsCol,
+  attachmentsColProperty,
+  replyTo,
+  async,
+}) => {
+  if (!host || !port || !username || !password || !senderEmail || !toEmail) {
+    throw new Error(
+      'Not all required e-mail parameters are filled in. Please check your SMTP settings and try again.'
+    );
+  }
+  let result = '';
+  const smtpCredentials = {
+    host,
+    port,
+    username,
+    password,
+    secure,
+  };
+
+  const engine = new Liquid();
+  engine.registerFilter('group', (collection, key) =>
+    _.groupBy(collection, key)
+  );
+
+  const renderedBody = engine.parseAndRenderSync(
+    body,
+    variables.reduce((ctx, { key, value }) => {
+      ctx[key] = value;
+      return ctx;
+    }, {})
+  );
+
+  const mappedAttachments = mapAttachments(
+    attachments,
+    attachmentsCol,
+    attachmentsColProperty
+  );
+
+  const mail = {
+    sender: {
+      from: senderName ? `"${senderName}" ${senderEmail}` : senderEmail,
+      name: senderName,
+      replyTo,
+    },
+    recipient: {
+      to: toName ? `"${toName}" ${toEmail}` : toEmail,
+      cc: cc,
+      bcc: bcc,
+    },
+    subject,
+    body: renderedBody,
+    attachments: mappedAttachments,
+  };
+
+  if (async) {
+    result = smtp(smtpCredentials, mail);
+  } else {
+    result = await smtp(smtpCredentials, mail);
+  }
+
+  return { result };
+};
+
+export default sendEmail;
